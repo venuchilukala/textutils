05030b256627b3273f2b11a2042e75399cc42aaf
venuchilukala <venuchilukala07@gmail.com>
2024-10-07 16:47:59 +0530

    Remove punctuations

--- textutils/urls.py
+++ textutils/urls.py
@@ -21,9 +21,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('removepunc', views.removepunc, name="rempun"),
-    path('capitalizefirst', views.capfirst, name="capfirst"),
-    path('newlineremove', views.newlineremove, name="newlineremove"),
-    path('spaceremove', views.spaceremove, name="spaceremove"),
-    path('charcount', views.charcount, name="charcount"),
+    path('analyze', views.analyze, name="analyze"),
+    # path('capitalizefirst', views.capfirst, name="capfirst"),
+    # path('newlineremove', views.newlineremove, name="newlineremove"),
+    # path('spaceremove', views.spaceremove, name="spaceremove"),
+    # path('charcount', views.charcount, name="charcount"),
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -4,23 +4,43 @@ from django.shortcuts import render
 
 
 def index(request):
-    
-    params = {'name' : 'venu', 'place' : 'hyderabad'}
-    
-    return render(request, 'index.html', params)
+    return render(request, 'index.html')
     # return HttpResponse('<h1>Home</h1>')
 
-def removepunc(request):
-    return HttpResponse('<p>Remove punctuation</p>')
+def analyze(request):
+    dj_text = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    print(dj_text)
+    print(removepunc)
+    
+    
+    if removepunc == 'on':
+        punctuations = '''.,?!:;'"()[]—-…/^_&'''
+        analyzed_text = ""
+        for char in dj_text:
+            if char not in punctuations:
+                analyzed_text += char 
+        print(analyzed_text)
+        
+        params ={
+            'purpose' : 'Removed Punctuations',
+            'analyzed_text' : analyzed_text
+        }
+        
+        return render(request,'analyze.html',params)
+    else:
+        return HttpResponse('Error')
 
-def capfirst(request):
-    return HttpResponse('<p>Capitalize first character</p>')
 
-def newlineremove(request):
-    return HttpResponse('<p>New line removed</p>')
 
-def spaceremove(request):
-    return HttpResponse('<p>Space is removed</p>')
+# def capfirst(request):
+#     return HttpResponse('<p>Capitalize first character</p>')
 
-def charcount(request):
-    return HttpResponse('<p>Character count</p>')
+# def newlineremove(request):
+#     return HttpResponse('<p>New line removed</p>')
+
+# def spaceremove(request):
+#     return HttpResponse('<p>Space is removed</p>')
+
+# def charcount(request):
+#     return HttpResponse('<p>Character count</p>')
