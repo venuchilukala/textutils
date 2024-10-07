bf32f5c70ba84cb16d00b48fc333a0a860a6d0e8
venuchilukala <venuchilukala07@gmail.com>
2024-10-07 17:37:29 +0530

    captalize, newlineremover, extraspaceremove, charcount

--- textutils/urls.py
+++ textutils/urls.py
@@ -22,8 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('analyze', views.analyze, name="analyze"),
-    # path('capitalizefirst', views.capfirst, name="capfirst"),
-    # path('newlineremove', views.newlineremove, name="newlineremove"),
-    # path('spaceremove', views.spaceremove, name="spaceremove"),
-    # path('charcount', views.charcount, name="charcount"),
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -8,39 +8,67 @@ def index(request):
     # return HttpResponse('<h1>Home</h1>')
 
 def analyze(request):
+    #Get text in the text area
     dj_text = request.GET.get('text', 'default')
+    
+    #Get status of check box of utils
     removepunc = request.GET.get('removepunc', 'off')
-    print(dj_text)
-    print(removepunc)
-    
-    
+    capsall = request.GET.get('capsall', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremove = request.GET.get('extraspaceremove', 'off')
+    charcount = request.GET.get('charcount', 'off')
+     
+   #Render the result
     if removepunc == 'on':
         punctuations = '''.,?!:;'"()[]—-…/^_&'''
         analyzed_text = ""
         for char in dj_text:
             if char not in punctuations:
-                analyzed_text += char 
-        print(analyzed_text)
-        
+                analyzed_text += char        
         params ={
             'purpose' : 'Removed Punctuations',
             'analyzed_text' : analyzed_text
         }
-        
         return render(request,'analyze.html',params)
+    
+    elif capsall == 'on':
+        analyzed_text = dj_text.upper()
+        params = {
+            'purpose' : 'Capitalized the Text',
+            'analyzed_text' : analyzed_text
+        }
+        return render(request, 'analyze.html', params)
+    
+    elif newlineremover == 'on':
+        analyzed_text = ''
+        for char in dj_text:
+            if char != '\n':
+                analyzed_text += char       
+        params = {
+            'purpose' : 'New Lines Removed',
+            'analyzed_text' : analyzed_text
+        }
+        return render(request, 'analyze.html', params)
+    
+    elif extraspaceremove == 'on':
+        analyzed_text = ''      
+        for index, char in enumerate(dj_text):
+            if not(dj_text[index] == ' ' and dj_text[index+1] == ' '):
+                analyzed_text += char         
+        params = {
+            'purpose' : 'Extra Spaces Removed',
+            'analyzed_text' : analyzed_text
+        }
+        return render(request, 'analyze.html', params)
+        
+    
+    elif charcount == 'on':
+        analyzed_text = 'The no of characters are : ' +  str(len(dj_text))
+        params = {
+            'purpose' : 'Extra Spaces Removed',
+            'analyzed_text' : analyzed_text
+        }    
+        return render(request, 'analyze.html', params)
+        
     else:
         return HttpResponse('Error')
-
-
-
-# def capfirst(request):
-#     return HttpResponse('<p>Capitalize first character</p>')
-
-# def newlineremove(request):
-#     return HttpResponse('<p>New line removed</p>')
-
-# def spaceremove(request):
-#     return HttpResponse('<p>Space is removed</p>')
-
-# def charcount(request):
-#     return HttpResponse('<p>Character count</p>')
